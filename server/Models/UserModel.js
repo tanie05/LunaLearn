08e377be6aa37c62c1125f81020337fd033598f7
RamneekBrar <rkbrar348@gmail.com>
2023-10-14 14:43:59 +0530

    Added Login/Signup Routes

---
+++ server/Models/UserModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        default: ""
+    },
+    role: {
+        type: String,
+        enum: ["Teacher", "Student"],
+        default: "Student"
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User
